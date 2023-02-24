6361305a2bed33346a2f746146d1edd9818fe494
marienv <mariela.n.vallejos@gmail.com>
2023-02-24 18:37:46 -0300

    multiple components

---
+++ 5_ReactBasics/multiplecomponents/src/App.js
@@ -0,0 +1,17 @@
+import Card from "./Card";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Task: Add three Card elements</h1>
+      <Card h2="First card's h2"/>
+      <Card h2="Second card's h2"/>
+      <Card h3="First card's h3"/>
+      <Card h3="Second card's h3"/>
+    </div>
+
+  );
+}
+
+export default App;
